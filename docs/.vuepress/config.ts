6296806a9554b1c28586e5b2609e50c88d94531b
Dmytro Zanuda <dmzanuda.work@gmail.com>
2024-02-16 13:38:43 +0200

    fix config

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -2,7 +2,7 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  base: "/",
+  base: "/db-course-hope-template/",
 
   lang: "en-US",
   title: "Назва проєкту",
